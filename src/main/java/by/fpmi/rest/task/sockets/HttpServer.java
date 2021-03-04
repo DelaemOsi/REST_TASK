581fd61642d49526c0e9ee0aeed14852c1dfcced
Andrey <Kuksa>
2021-03-04 13:44:45 +0300

    Added executor service with fixed thread pool

--- src/main/java/by/fpmi/rest/task/sockets/HttpServer.java
+++ src/main/java/by/fpmi/rest/task/sockets/HttpServer.java
@@ -4,13 +4,19 @@ package by.fpmi.rest.task.sockets;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HttpServer {
+
+    private final static int THREADS_COUNT = 32;
+
     public void run() throws IOException {
         ServerSocket serverSocket = new ServerSocket(8080);
+        ExecutorService service = Executors.newFixedThreadPool(THREADS_COUNT);
         while (true) {
             Socket clientSocket = serverSocket.accept();
-            new Thread(new SocketProcessor(clientSocket)).start();
+            service.execute(new SocketProcessor(clientSocket));
         }
     }
 
